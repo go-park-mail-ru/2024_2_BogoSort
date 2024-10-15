066c798d292d3441fd5ab4017c79758a7355b1a5
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-10-15 15:12:48 +0300

    added: session model

--- internal/pkg/domain/auth.go
+++ internal/pkg/domain/auth.go
@@ -1,18 +1,5 @@
 package domain
 
-import "sync"
-
-type Sessions struct {
-	Sessions map[string]string
-	Mu       sync.Mutex
-}
-
-type SessionRepository interface {
-	AddSession(email string) string
-	RemoveSession(email string) error
-	SessionExists(sessionID string) bool
-}
-
 type AuthData struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
---
+++ internal/pkg/domain/session.go
@@ -0,0 +1,14 @@
+package domain
+
+import "sync"
+
+type Sessions struct {
+	Sessions map[string]string
+	Mu       sync.Mutex
+}
+
+type SessionRepository interface {
+	AddSession(email string) string
+	RemoveSession(email string) error
+	SessionExists(sessionID string) bool
+}
