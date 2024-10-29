228ea0a13324dad2843b7464b7a0596b0f6143dd
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-10-29 20:40:46 +0300

    updated: user dto model

--- internal/entity/dto/user.go
+++ internal/entity/dto/user.go
@@ -1,9 +1,12 @@
 package dto
 
+import "github.com/google/uuid"
+
 type User struct {
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Phone    string `json:"phone"`
-	AvatarId string `json:"avatar_id"`
-	Status   int    `json:"status"`
+	ID       uuid.UUID `json:"id"`
+	Email    string    `json:"email"`
+	Username string    `json:"username"`
+	Phone    string    `json:"phone"`
+	AvatarId string    `json:"avatar_id"`
+	Status   int       `json:"status"`
 }
