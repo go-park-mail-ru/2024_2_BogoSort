cdf76c3690c1accb8286a45908a4d962f87c7e64
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-11-15 02:13:17 +0300

    implemented: grpc auth methods

--- internal/delivery/grpc/auth/client.go
+++ internal/delivery/grpc/auth/client.go
@@ -1,1 +1,60 @@
 package auth
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	authProto "github.com/go-park-mail-ru/2024_2_BogoSort/internal/delivery/grpc/auth/proto"
+)
+
+type GrpcClient struct {
+	authManager authProto.AuthServiceClient
+}
+
+func NewGrpcClient(addr string) (*GrpcClient, error) {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	authManager := authProto.NewAuthServiceClient(conn)
+
+	_, err = authManager.Ping(context.Background(), &authProto.NoContent{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &GrpcClient{authManager: authManager}, nil
+}
+
+func (c *GrpcClient) GetUserIDBySession(sessionID string) (string, error) {
+	user, err := c.authManager.GetUserIDBySession(context.Background(), &authProto.Session{Id: sessionID})
+	if err != nil {
+		return "", err
+	}
+	return user.Id, nil
+}
+
+func (c *GrpcClient) CreateSession(userID uuid.UUID) (string, error) {
+	session, err := c.authManager.CreateSession(context.Background(), &authProto.User{Id: userID.String()})
+	if err != nil {
+		return "", err
+	}
+	return session.Id, nil
+}
+
+func (c *GrpcClient) DeleteSession(sessionID string) error {
+	_, err := c.authManager.DeleteSession(context.Background(), &authProto.Session{Id: sessionID})
+	return err
+}
+
+func (c *GrpcClient) SetSession(sessionID string, sessionAliveTime int, secureCookie bool) (string, error) {
+	cookie, err := c.authManager.SetSession(context.Background(), &authProto.Session{Id: sessionID}, sessionAliveTime, secureCookie)
+	if err != nil {
+		return "", err
+	}
+	return cookie.Value, nil
+}
--- internal/delivery/grpc/auth/server.go
+++ internal/delivery/grpc/auth/server.go
@@ -1,1 +1,52 @@
 package auth
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	authProto "github.com/go-park-mail-ru/2024_2_BogoSort/internal/delivery/grpc/auth/proto"
+
+	"github.com/go-park-mail-ru/2024_2_BogoSort/internal/usecase"
+)
+
+type GrpsServer struct {
+	authProto.UnimplementedAuthServiceServer
+	AuthUC usecase.Auth
+}
+
+func NewGrpsServer(authUC usecase.Auth) *GrpsServer {
+	return &GrpsServer{AuthUC: authUC}
+}
+
+func (s *GrpsServer) GetUserIDBySession(_ context.Context, in *authProto.Session) (*authProto.User, error) {
+	userID, err := s.AuthUC.GetUserIdBySession(in.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &authProto.User{Id: userID.String()}, nil
+}
+
+func (s *GrpsServer) CreateSession(_ context.Context, in *authProto.User) (*authProto.Session, error) {
+	userID, err := uuid.Parse(in.Id)
+	if err != nil {
+		return nil, err
+	}
+	sessionID, err := s.AuthUC.CreateSession(userID)
+	if err != nil {
+		return nil, err
+	}
+	return &authProto.Session{Id: sessionID}, nil
+}
+
+func (s *GrpsServer) DeleteSession(_ context.Context, in *authProto.Session) (*authProto.NoContent, error) {
+	err := s.AuthUC.Logout(in.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &authProto.NoContent{}, nil
+}
+
+func (s *GrpsServer) Ping(_ context.Context, _ *authProto.NoContent) (*authProto.NoContent, error) {
+	return &authProto.NoContent{}, nil
+}
