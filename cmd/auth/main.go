671e7ba8f9d5093c37c7dbee673b4abe74dec3f1
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-11-17 03:07:35 +0300

    updated: auth server

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -13,53 +13,51 @@ import (
 	"github.com/go-park-mail-ru/2024_2_BogoSort/pkg/connector"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthProto "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
-	// Инициализация логгера
 	logger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatalf("Не удалось создать логгер: %v", err)
 	}
 	defer logger.Sync()
 
-	// Инициализация конфигурации
 	cfg, err := config.Init()
 	if err != nil {
 		logger.Fatal("Ошибка при инициализации конфигурации", zap.Error(err))
 	}
 
-	// Подключение к Redis
 	rdb, err := connector.GetRedisConnector(cfg.RdAddr, cfg.RdPass, cfg.RdDB)
 	if err != nil {
-		logger.Fatal("Ошибка при подключении к Redis", zap.Error(err))
+		logger.Fatal("Ошибка при подключении �� Redis", zap.Error(err))
 	}
 
-	// Инициализация репозитория сессий
 	sessionRepo, err := redis.NewSessionRepository(rdb, 10, context.Background(), logger)
 	if err != nil {
 		logger.Fatal("Ошибка при инициализации репозитория сессий", zap.Error(err))
 	}
 
-	// Инициализация сервисов
 	authService := service.NewAuthService(sessionRepo, logger)
 
-	// Создание gRPC сервера
-	grpcServer := grpc.NewServer()
-	authServer := auth.NewGrpsServer(authService)
+	server := grpc.NewServer()
+	authServer := auth.NewGrpcServer(authService)
 
-	// Регистрация AuthService сервера
-	authProto.RegisterAuthServiceServer(grpcServer, authServer)
+	healthServer := health.NewServer()
+	healthProto.RegisterHealthServer(server, healthServer)
+	healthServer.SetServingStatus("", healthProto.HealthCheckResponse_SERVING)
 
-	// Настройка прослушивания порта
-	address := ":50051" // Измените на нужный порт при необходимости
+	authProto.RegisterAuthServiceServer(server, authServer)
+
+	address := config.GetAuthAddress()
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		logger.Fatal("Не удалось прослушивать порт", zap.Error(err))
 	}
 
 	logger.Info("Auth сервер запущен на " + address)
-	if err := grpcServer.Serve(lis); err != nil {
+	if err := server.Serve(lis); err != nil {
 		logger.Fatal("Ошибка при запуске gRPC сервера", zap.Error(err))
 	}
 }
