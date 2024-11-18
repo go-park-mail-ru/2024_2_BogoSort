99d0017ecee482e1dff2e7b856d7d3a53d179e7a
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-11-18 22:39:25 +0300

    added: cart purchase server start, changed: auth logging

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 	"net"
 
 	"github.com/go-park-mail-ru/2024_2_BogoSort/config"
@@ -18,28 +17,25 @@ import (
 )
 
 func main() {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatalf("Не удалось создать логгер: %v", err)
-	}
-	defer logger.Sync()
+	zap.ReplaceGlobals(zap.Must(zap.NewProduction()))
+	defer zap.L().Sync()
 
 	cfg, err := config.Init()
 	if err != nil {
-		logger.Fatal("Ошибка при инициализации конфигурации", zap.Error(err))
+		zap.L().Fatal("Ошибка при инициализации конфигурации", zap.Error(err))
 	}
 
 	rdb, err := connector.GetRedisConnector(cfg.RdAddr, cfg.RdPass, cfg.RdDB)
 	if err != nil {
-		logger.Fatal("Ошибка при подключении �� Redis", zap.Error(err))
+		zap.L().Fatal("Ошибка при подключении к Redis", zap.Error(err))
 	}
 
-	sessionRepo, err := redis.NewSessionRepository(rdb, int(cfg.Session.ExpirationTime.Seconds()), context.Background(), logger)
+	sessionRepo, err := redis.NewSessionRepository(rdb, int(cfg.Session.ExpirationTime.Seconds()), context.Background(), zap.L())
 	if err != nil {
-		logger.Fatal("Ошибка при инициализации репозитория сессий", zap.Error(err))
+		zap.L().Fatal("Ошибка при инициализации репозитория сессий", zap.Error(err))
 	}
 
-	authService := service.NewAuthService(sessionRepo, logger)
+	authService := service.NewAuthService(sessionRepo, zap.L())
 
 	server := grpc.NewServer()
 	authServer := auth.NewGrpcServer(authService)
@@ -53,11 +49,11 @@ func main() {
 	address := config.GetAuthAddress()
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
-		logger.Fatal("Не удалось прослушивать порт", zap.Error(err))
+		zap.L().Fatal("Не удалось прослушивать порт", zap.Error(err))
 	}
 
-	logger.Info("Auth сервер запущен на " + address)
+	zap.L().Info("Auth сервер запущен на " + address)
 	if err := server.Serve(lis); err != nil {
-		logger.Fatal("Ошибка при запуске gRPC сервера", zap.Error(err))
+		zap.L().Fatal("Ошибка при запуске gRPC сервера", zap.Error(err))
 	}
 }
