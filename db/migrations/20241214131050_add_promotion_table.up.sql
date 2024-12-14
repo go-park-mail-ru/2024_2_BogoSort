8863b1f77586f190ed80ce924818eb0a072dcf3c
ksuysshaa <k.ageeva0505@gmail.com>
2024-12-14 21:35:51 +0300

    added: migration

--- db/migrations/20241214131050_add_promotion_table.up.sql
+++ db/migrations/20241214131050_add_promotion_table.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE promotion (
     id SERIAL PRIMARY KEY,
-    amount VARCHAR(255) DEFAULT '50',
-    days INT DEFAULT 7
+    amount VARCHAR(255) NOT NULL,
+    days INT NOT NULL DEFAULT 7
 );
+
+INSERT INTO promotion (amount, days) VALUES ('50.00', 7);
