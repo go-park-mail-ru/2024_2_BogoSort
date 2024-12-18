a1573f51dc51d7fe7df873849e22719f994e680f
qosmioo <79136983+qosmioo@users.noreply.github.com>
2024-12-18 10:41:13 +0300

    added: httpjson

---
+++ internal/delivery/http/utils/httpJSON.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/mailru/easyjson"
+)
+
+func WriteJSON(w http.ResponseWriter, v easyjson.Marshaler, status int) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	writer := io.Writer(w)
+	_, err := easyjson.MarshalToWriter(v, writer)
+	return err
+}
+
+func ReadJSON(r *http.Request, v easyjson.Unmarshaler) error {
+	if err := easyjson.UnmarshalFromReader(r.Body, v); err != nil {
+		return err
+	}
+	return r.Body.Close()
+}
